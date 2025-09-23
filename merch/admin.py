6a69ebc317d47f2a3b19589ae7c117ba972485f7
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-09-23 17:33:05 +0100

    customise admin

--- merch/admin.py
+++ merch/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Merch, Category
 
-# Register your models here.
+class MerchAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Merch, MerchAdmin)
+admin.site.register(Category, CategoryAdmin)
