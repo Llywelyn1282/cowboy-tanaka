44cdce76188d61b3c748503066f6e16cfe9f2daa
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-09-24 14:34:37 +0100

    add search functionality for merch

--- merch/views.py
+++ merch/views.py
@@ -1,21 +1,36 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Merch
 
+# Create your views here.
 
 def all_merch(request):
     """ A view to show all merch, including sorting and search queries """
 
     merch = Merch.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('merch'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            merch = merch.filter(queries)
 
     context = {
         'merch': merch,
+        'search_term': query,
     }
 
     return render(request, 'merch/merch.html', context)
 
 
 def merch_detail(request, merch_id):
-    """ A view to show individual merch detail """
+    """ A view to show individual merch item details """
 
     merch = get_object_or_404(Merch, pk=merch_id)
 
