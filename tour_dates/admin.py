c1371aa2c7eca14feb7f7801986f71823696a725
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-09-26 15:58:59 +0100

    add tour dates view and admin

--- tour_dates/admin.py
+++ tour_dates/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Tour_Dates
 
-# Register your models here.
+class Tour_Dates_Admin(admin.ModelAdmin):
+    list_display = (
+        'date',
+        'venue',
+        'location',
+        'support_act',
+        'image',
+    )
+
+    ordering = ('date',)
+
+
+admin.site.register(Tour_Dates, Tour_Dates_Admin)
