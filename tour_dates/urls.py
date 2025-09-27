c54eff9c884b875cc825e3678313208ddf0c3537
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-09-27 16:07:01 +0100

    add text to info page, add event detail page to tour dates

--- tour_dates/urls.py
+++ tour_dates/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_tour_dates, name='tour_dates'),
+    path('<int:tour_dates_id>', views.event_detail, name='event_detail')
 ]
--- tour_dates/views.py
+++ tour_dates/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Tour_Dates
 
 
@@ -12,3 +12,15 @@ def all_tour_dates(request):
     }
 
     return render(request, 'tour_dates/tour_dates.html', context)
+
+
+def event_detail(request, tour_dates_id):
+    """ A view to show individual news post details """
+
+    tour_dates = get_object_or_404(Tour_Dates, pk=tour_dates_id)
+
+    context = {
+        'tour_dates': tour_dates,
+    }
+
+    return render(request, 'tour_dates/event_detail.html', context)
