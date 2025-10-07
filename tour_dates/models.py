25cd1811e40481b84065db65392d1e54bbe90b67
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-10-07 23:42:55 +0100

    final commit for first submission (no time, no mentors, no support)

--- tour_dates/models.py
+++ tour_dates/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Tour_Dates(models.Model):
     class Meta:
         verbose_name_plural = 'Tour Dates'
+        ordering = ['date']
 
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
