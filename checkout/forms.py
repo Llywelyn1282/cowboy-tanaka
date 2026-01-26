dfbfb09ab8f3ecb3d619ddaee87b2c14fd3feeb8
Llywelyn1282 <gwilymllywelyn@gmail.com>
2026-01-26 21:30:08 +0000

    reformatting attempt #2

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,25 +1,27 @@
 from django import forms
-from django_countries.widgets import CountrySelectWidget
 from .models import Order
 
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = (
-            'full_name', 'email', 'phone_number',
-            'street_address1', 'street_address2',
-            'town_or_city', 'postcode', 'country',
-            'county',
-        )
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
         widgets = {
-            'country': CountrySelectWidget(),
+            'country': forms.Select(attrs={
+                'aria-label': 'Country'
+            }),
         }
 
     def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
         super().__init__(*args, **kwargs)
-
-        field_config = {
+        placeholders = {
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
@@ -28,22 +30,15 @@ class OrderForm(forms.ModelForm):
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County, State or Locality',
-            'country': 'Country',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
-
-        for name, field in self.fields.items():
-            label = field_config.get(name, name.replace('_', ' ').title())
-
-            # Add accessible name (required since labels are hidden)
-            field.widget.attrs['aria-label'] = label
-
-            # Add placeholder only to text inputs
-            if name != 'country':
-                if field.required:
-                    label = f'{label} *'
-                field.widget.attrs['placeholder'] = label
-
-            field.widget.attrs['class'] = 'stripe-style-input'
-            field.label = False
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
